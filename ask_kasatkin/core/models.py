6cb99bf99d162e995a26d5d4f0d190d06fbb7df5
Vladimir <volkvid@yandex.ru>
2015-03-03 00:04:05 +0300

    update in DB (in model) + registration method (without avatar yet, with some bugs)

--- ask_kasatkin/core/models.py
+++ ask_kasatkin/core/models.py
@@ -1,42 +1,51 @@
 # coding:utf8
 from django.db import models
+from django.contrib.auth.models import User
 
-# the user :)
-class the_user(models.Model):
-    email = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=100)
-    login = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    avatar_link = models.CharField(max_length=200) # link to static file on the f-server ?
-    date = models.DateTimeField("user was registered")
+import datetime
+
+class user_properties(models.Model):
+    user = models.ForeignKey(User)
+    nickname = models.CharField(max_length=100)  # 2 unique fields
     rating = models.IntegerField(default=0)
-
-
-class the_tag(models.Model):
-    name = models.CharField(max_length=100)
-    # to show the most popular tag -- increased when tag was added
-    used_times = models.IntegerField(default=1)
 
 
 class the_question(models.Model):
     title = models.CharField(max_length=250)
     text = models.TextField()
-    date = models.DateTimeField("question date")
     rating = models.IntegerField(default=0)
+    the_answer_was_chosen = models.IntegerField(default=0) # when author chooses an answer set 1
+    author = models.ForeignKey(User)
 
-    author = models.ForeignKey(the_user)
-    tags = models.ForeignKey(the_tag)
-
-    # when author chooses an answer set this 1
-    the_answer_was_chosen = models.IntegerField(default=0)
+    date = models.DateTimeField(editable=False)
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.date = datetime.datetime.today()
+        return super(the_question, self).save(*args, **kwargs)
 
 
 class the_answer(models.Model):
     text = models.TextField()
-    date = models.DateTimeField("answer date")
     rating = models.IntegerField(default=0)
-    # + only one answer can be marked in one question
-    is_marked_as_true = models.BooleanField(default=False)
-
-    author = models.ForeignKey(the_user)
+    is_marked_as_true = models.BooleanField(default=False) # + only one answer can be marked in one question
+    author = models.ForeignKey(User)
     contributed_to = models.ForeignKey(the_question) # which question to answer
+
+    date = models.DateTimeField(editable=False)
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.date = datetime.datetime.today()
+        return super(the_answer, self).save(*args, **kwargs)
+
+
+class the_tag(models.Model):
+    name = models.CharField(max_length=100)
+    used_in = models.ForeignKey(the_question)
+
+
+#       add class to store
+#      votes of the members
+#       /               \
+# to questions      to answers
--- ask_kasatkin/core/views.py
+++ ask_kasatkin/core/views.py
@@ -1,9 +1,10 @@
 # coding:utf8
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from django.template import loader, RequestContext
+from django.contrib.auth.models import User
+from core.models import user_properties
 
+from django.http import HttpResponse
 from random import randint  # used in demo
 
 # -- gets new questions, etc...
@@ -26,7 +27,8 @@ def index_page(request):
         {
             "title": "what's wrong with my django-app urls?",
             "link": "link_to_open_the_question_thread_by_ID_i_think",
-            "text": "When you use a ModelForm, the call to is_valid() will perform these validation steps for all the fields that are included on the form. See the ModelForm documentation for more information. You should only need to call a model’s full_clean() method if you plan to handle validation errors yourself, or if you have excluded fields from the ModelForm that require validation."[:400] + "...",  # first 400 chars e.g
+            "text": "When you use a ModelForm, the call to is_valid() will perform these validation steps for all the fields that are included on the form. See the ModelForm documentation for more information. You should only need to call a model’s full_clean() method if you plan to handle validation errors yourself, or if you have excluded fields from the ModelForm that require validation."[
+                    :400] + "...",  # first 400 chars e.g
             # "date": "28.02.15 ",  # no data in block, needed for sorting only
             "rating": 3,
             "answers": 2,  # contributed answers
@@ -36,29 +38,30 @@ def index_page(request):
             "avatar": "Vladimir.jpg"
         },
         {
-            "title": "long-long title test | "*7,
+            "title": "long-long title test | " * 7,
             "link": "link_to_open_the_question_thread_by_ID_i_think",
-            "text": "See the ModelForm documentation for more information. You should only need to call a model’s full_clean() method if you plan to handle validation errors yourself, or if you have excluded fields from the ModelForm that require validation."[:400] + "...",  # first 400 chars e.g
+            "text": "See the ModelForm documentation for more information. You should only need to call a model’s full_clean() method if you plan to handle validation errors yourself, or if you have excluded fields from the ModelForm that require validation."[
+                    :400] + "...",  # first 400 chars e.g
             # "date": "28.02.15 ",  # no data in block, needed for sorting only
             "rating": 341550,
             "answers": 200124,  # contributed answers
-            "tags": ["Long tags are rather cool but not in Bootstrap :)", "Testing tempate with big footer", "noSQL is here"],  # 3 tags - MAX
+            "tags": ["Long tags are rather cool but not in Bootstrap :)", "Testing tempate with big footer",
+                     "noSQL is here"],  # 3 tags - MAX
 
             "author": "1335",
             "avatar": "1335.jpg"
         }
     ]
-    # make sure the user is logged
+    # make sure the user is logged:
     if request.user.is_authenticated():
-        # load from model ...
         data["personal"] = {
-            "nickname": "Vladimir",
-            "avatar": "Vladimir.jpg"
+            "nickname": request.user.nickname,
+            "avatar": str(request.user.id) + ".jpg"
         }
     return render(request, "core/templates/index.html", data)
 
 
-# render login page
+# render login page - OK
 def show_login(request):
     return render(request, "core/templates/login.html", get_static_data())
 
@@ -74,20 +77,19 @@ def validate_login(request):
         elif len(rec_passw) == 0:
             data["error"] = {"title": "No password", "text": "Please enter your password and try again"}
         else:
-            # do login stuff ...
             user = authenticate(username=rec_login, password=rec_passw)
-            if user: # - cool :)
+            if user:
                 login(request, user)
-                return index_page(request) #render(request, "core/templates/index.html", data)
+                return index_page(request)
             else:
-                data["error"] = {"title": str(user), "text": "No such user"}
+                data["error"] = {"title": "No such user", "text": ""}
     else:
         data["error"] = {"title": "Wrong request", "text": "You should use POST-requests only to login"}
     # returns error message
     return render(request, "core/templates/login.html", data)
 
 
-# render registration page
+# render registration page - OK
 def register(request):
     return render(request, "core/templates/register.html", get_static_data())
 
@@ -102,18 +104,46 @@ def validate_register(request):
         password1_ = request.POST['input_password']
         password2_ = request.POST['input_password_rep']
 
-        # a) check all fields are filled
-        # b) work with file ...
-        # c) OK - create a new user, return main page
-        # d) send an error msg, return the same page
+        if len(login_) < 6:
+            data["error"] = {"title": "Login-field", "text": "Please use login at least 6 symbols long"}
+        elif len(nickname_) < 3:
+            data["error"] = {"title": "Nickname-field", "text": "Please use nickname at least 3 symbols long"}
+        elif len(password1_) < 6:
+            data["error"] = {"title": "Password-field", "text": "Please use password at least 6 symbols long"}
+        else:
+            if validate_new_email(email_):
+                if password1_ == password2_:
+                    # check if no such login
+                    # check if no such nickname
+                    # check if no such email
 
+                    user = User()
+                    user.email = email_
+                    user.username = login_
+                    user.password = password1_
+                    user.save()
+
+                    props = user_properties()
+                    props.user = user
+                    props.nickname = nickname_
+                    props.save()
+
+                    # return render(request, "core/templates/index.html", data)
+                    #user = authenticate(username=login_, password=password1_)
+                    #login(request, user)
+                    return index_page(request)
+                else:
+                    data["error"] = {"title": "Passwords don't match",
+                                     "text": "Please be careful on typing your password"}
+            else:
+                data["error"] = {"title": "Incorrect email", "text": "Please use a valid email"}
     else:
         data["error"] = {"title": "Wrong request", "text": "You should use POST-requests only to login"}
     # returns error message
     return render(request, "core/templates/register.html", data)
 
 
-# -- shown only for logged users:
+# shown only for logged users - OK
 def self_logout(request):
     logout(request)
     return index_page(request)
@@ -132,15 +162,20 @@ def search(request):
     return HttpResponse("JSON result ... ")
 
 
+# validation used in registration
+def validate_new_email(email):
+    if len(email) > 4:
+        return True
+    else:
+        return False
 
-# -- ALL METHODS CALL THIS! (static demo now)
 
 # returns popular tags from file ? cache
 def get_static_data():
     res = []
     append = res.append
-    demo_pop_tags = ["Technopark", "Baumanka", "C", "Python", "MySQL", "Ruby", "apple", "iOS", "swift", "django", "php", "flask",
-                     "objective-c", "Ubuntu-server", "VPS", "Coffee Script", "sudo"]
+    demo_pop_tags = ["Technopark", "Baumanka", "C", "Python", "MySQL", "Ruby", "apple", "iOS", "swift", "django",
+                     "php", "flask", "objective-c", "Ubuntu-server", "VPS", "Coffee Script", "sudo"]
     demo_labels = ["label label-default", "label label-primary", "label label-success",
                    "label label-info", "label label-warning", "label label-danger"]
     for tag in demo_pop_tags:

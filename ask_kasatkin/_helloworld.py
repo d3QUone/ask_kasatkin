9058406642492976746809f7127c218c30b89661
Vladimir <volkvid@yandex.ru>
2015-03-09 15:29:37 +0300

    WSGI-script updated

--- ask_kasatkin/_helloworld.py
+++ ask_kasatkin/_helloworld.py
@@ -5,13 +5,14 @@
 # to check: localhost:8000
 #
 
+# or just 'sudo service gunicorn start / restart
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
+    start_response('200 OK', [('Content-Type', 'text/html')])
 
-    # shows ALL request parameters in a heap 
-    keys = env.keys()
+    # keys = env.keys() # shows ALL request parameters in a heap
+    keys = ["REQUEST_METHOD", "PATH_INFO", "QUERY_STRING"]
     out = ""
     for i in keys:
-        out += "<strong>{0}</strong> | {1} <br>".format(i, env[i])
-    
-    return ["Hello World<hr>", out]
+        out += "<strong>{0}:</strong> {1}<br>\n".format(i, env[i])
+
+    return ["Hello World<hr>\n", out]

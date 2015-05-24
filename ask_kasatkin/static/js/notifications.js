c72d16b529ed8b96e9fa56cf6da2b84f95f259c1
Vladimir <volkvid@yandex.ru>
2015-05-24 16:51:14 +0300

    fixed

--- ask_kasatkin/static/js/notifications.js
+++ ask_kasatkin/static/js/notifications.js
@@ -3,7 +3,7 @@ var ws = new WebSocket("ws://vksmm.info:8888/ws/" + channel_id);
 
 ws.onmessage = function(ev){
   var json = JSON.parse(ev.data);
-  var new_ans = '<a name="answer_' + json.id + '"></a><div class="row question__block fresh"><div class="col-xs-2 question__left"><img class="img-rounded question__avatar" src="/uploads/' + json.avatar + '"><div id="' + json.id + '" class="rating__info answer_rating">Rating: 0</div><div class="rating__like__block fresh__like"><span id="' + json.id + '" class="like__button ans_like">+</span><span id="' + json.id + '" class="like__button ans_dislike">-</span></div></div><div class="col-xs-10 question__right"><p>' + json.text + '</p><div class="question__tags"><strong>Author:</strong> ' + json.nickname + ' </div></div></div><br>';
+  var new_ans = '<a name="answer_' + json.id + '"></a><div class="row question__block fresh"><div class="col-xs-2 question__left"><img class="img-rounded question__avatar" src="/uploads/' + json.avatar + '"><div id="' + json.id + '" class="rating__info answer_rating">Rating: 0</div><div class="fresh__like"><span id="' + json.id + '" class="like__button ans_like">+</span><span id="' + json.id + '" class="like__button ans_dislike">-</span></div></div><div class="col-xs-10 question__right"><p>' + json.text + '</p><div class="question__tags"><strong>Author:</strong> ' + json.nickname + ' </div></div></div><br>';
   $('div#fresh_answers').append(new_ans);
 };
 

092550136b628c68d241074a50d5441a377bf8b6
Vladimir <volkvid@yandex.ru>
2015-05-24 14:51:48 +0300

    fixed notification JS

--- ask_kasatkin/static/js/notifications.js
+++ ask_kasatkin/static/js/notifications.js
@@ -13,26 +13,7 @@ ws.onmessage = function(ev){
   $message.text('recieved message');
 
   var json = JSON.parse(ev.data);
-  var new_ans = '<a name="answer_' + json.id + '"></a>
-        <div class="row question__block fresh">
-            <div class="col-xs-2 question__left">
-                <img class="img-rounded question__avatar" src="/uploads/' + json.avatar + '">
-                <div id="' + json.id + '" class="rating__info answer_rating">
-                    Rating: 0
-                </div>
-                <div class="rating__like__block">
-                    <span id="' + json.id + '" class="like__button ans_like">+</span>
-                    <span id="' + json.id + '" class="like__button ans_dislike">-</span>
-                </div>
-            </div>
-            <div class="col-xs-10 question__right">
-                <p>' + json.text + '</p>
-                <div class="question__tags">
-                    <strong>Author:</strong>' + json.nickname + '
-                </div>
-            </div>
-        </div>'
-
+  var new_ans = '<a name="answer_' + json.id + '"></a><div class="row question__block fresh"><div class="col-xs-2 question__left"><img class="img-rounded question__avatar" src="/uploads/' + json.avatar + '"><div id="' + json.id + '" class="rating__info answer_rating">Rating: 0</div><div class="rating__like__block"><span id="' + json.id + '" class="like__button ans_like">+</span><span id="' + json.id + '" class="like__button ans_dislike">-</span></div></div><div class="col-xs-10 question__right"><p>' + json.text + '</p><div class="question__tags"><strong>Author:</strong>' + json.nickname + '</div></div></div>';
   $('div#fresh_answers').append(new_ans);
 };
 ws.onclose = function(ev){

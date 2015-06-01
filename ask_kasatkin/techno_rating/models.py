c79e43668321f98bc7edc5aa70b75bffe1f8ef07
Vladimir <volkvid@yandex.ru>
2015-06-02 00:15:11 +0300

    init main model

--- ask_kasatkin/techno_rating/models.py
+++ ask_kasatkin/techno_rating/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# techno-start idea
+class TechIdea(models.Model):
+	name = models.CharField(max_length=300)
+	link = models.CharField(max_length=300)  # full url
+	like = models.IntegerField(default=0)
+	comm = models.IntegerField(default=0)
+		

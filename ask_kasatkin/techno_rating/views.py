231850e09f58b53c7c401405abd074271be2ed74
Vladimir <volkvid@yandex.ru>
2015-06-06 21:16:23 +0300

    simplified controller

--- ask_kasatkin/techno_rating/views.py
+++ ask_kasatkin/techno_rating/views.py
@@ -5,27 +5,9 @@ reload(sys)
 sys.setdefaultencoding('utf8') # make UTF 8 global-hack
 
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage
-from django.http import Http404
 from techno_rating.models import TechIdea
 
 
 def index(request):
-    try:
-        page = int(request.GET.get("page", "1"))
-    except ValueError:
-        raise Http404
-
-    # query = request.GET.get("sort", "latest")
-    # if query != "popular":
-    #     paginator = Paginator(TechIdea.objects.all().order_by('-id'), 30)
-    # else:
-    #     paginator = Paginator(TechIdea.objects.all().order_by('-like'), 30)
-
-    paginator = Paginator(TechIdea.objects.all().order_by('-like'), 30)
-    try:
-        data = paginator.page(page)
-    except EmptyPage:
-        data = paginator.page(paginator.num_pages)
-
+    data = TechIdea.objects.all().order_by('-like')[:100]
     return render(request, "rating_page.html", {"data": data})
